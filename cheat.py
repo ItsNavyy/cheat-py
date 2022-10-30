c5ffe2bf4f611dd6318354657354457de9f9acd6
Navy <116895994+ItsNavyy@users.noreply.github.com>
2022-10-30 12:35:46 +0700

    Create cheat.py

---
+++ cheat.py
@@ -0,0 +1,58 @@
+
+import time, os, sys
+
+os.system("title [ ! ] Halaman Login [ ! ] ")
+
+password = input("Passwordnya Coy: ")
+
+if password == "anakbangsa":
+    print("Selamat ngecheat")
+    time.sleep(1.5)
+else:
+    print()
+    time.sleep(10000000000)
+
+os.system("cls")
+    
+
+os.system("title [ ! ] Cheat Jawaban Apapun [ ! ]")
+try:
+    from brainly_scraper import brainly
+    from art import *
+except ModuleNotFoundError:
+    os.system('pip install art')
+    time.sleep(1)
+    os.system('python -m pip install brainly_scraper')
+
+def utama():
+    try:
+        pertanyaan = input('Masukan pertanyaan mu: ')
+        jumlah_pertanyaan = int(input('Jumlah pertanyaan(abaikan jika tidak perlu): '))
+    except ValueError:
+        jumlah_pertanyaan=1
+
+    os.system('cls')
+    tprint('Jawaban')
+    print('                                            \033[32mPertanyaan : {0}\n                                            \033[32mJumlah pertanyaan : {1}'.format(pertanyaan,jumlah_pertanyaan))
+    scrap=brainly("""{0}""".format(pertanyaan), jumlah_pertanyaan)
+
+    for i in scrap:
+        type_slowly(f"\033[91mBeberapa pertanyaan yang sama:\n\033[32m{i.question.content}", .03)
+        for answer in enumerate(i.answers):
+            type_slowly(f"\033[91mJawaban dari pertanyaan mu:\n\033[32m{answer[1].content}", .03)
+
+def type_slowly(text, speed, newLine = True):
+    for i in text: 
+        print(i, end = "", flush = True) 
+        time.sleep(speed) 
+    if newLine:
+        print() 
+
+utama()
+while True:       
+    tanya_kembali = input('\nJawab y untuk kembali bertanya jika tidak Tekan saja: ')
+    if tanya_kembali == 'y':
+        os.system('cls')
+        utama()
+    else:
+        sys.exit()
